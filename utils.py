2a56384e61234b368199379e14e62cf7315414d9
yaolinxia <18860976931@163.com>
2018-10-13 22:21:10 +0800

    image show and load wrapping

--- utils.py
+++ utils.py
@@ -1,4 +1,8 @@
 import cv2 as cv
+import numpy as np
+from matplotlib import pyplot as plt
+
+
 import json
 import interference as itf
 
@@ -56,3 +60,12 @@ def save_sample(file_path: str, img):
     :return:
     """
     cv.imencode('.jpg', img)[1].tofile(file_path)
+
+def read_img(file_path: str):
+    return cv.imdecode(np.fromfile(file_path, dtype=np.uint8), 0)
+
+
+def show_img(img):
+    plt.imshow(img, cmap='gray', interpolation='bicubic')
+    plt.xticks([]), plt.yticks([])  # to hide tick values on X and Y axis
+    plt.show()
