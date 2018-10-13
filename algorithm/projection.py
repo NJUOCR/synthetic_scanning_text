10d36fa8f57a1aec7f8935a1233569a22af84678
AssKickerSurfaceBook <121584565@163.com>
2018-10-13 20:46:21 +0800

    projective histogram task

--- algorithm/projection.py
+++ algorithm/projection.py
@@ -3,10 +3,23 @@ Please implement `projective histogram` here
 """
 
 
-def project(img, along='both'):
+def project(img, direction='vertical'):
     """
     Do projection.
     :param img: A numpy array. source image.
-    :param along: `vertical` | `horizontal` | `both`
-    :return:
+    :param direction: `vertical` | `horizontal`
+    :return: A numpy array with shape (1, )
+    """
+
+
+def draw_projective_histogram(img, direction='both'):
+    """
+    1. Copy the input img array
+    2. Do padding, on right, bottom or both, according to the `direction`
+    3. Draw histogram
+
+    > The original input image will not be changed.
+    :param img: A numpy array, the source image.
+    :param direction: `vertical` | `horizontal` | `both`(default)
+    :return: A numpy array
     """
