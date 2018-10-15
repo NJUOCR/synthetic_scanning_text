85a20ba830e23e35fb61a5c588e0a18c62ebd068
yaolinxia <18860976931@163.com>
2018-10-15 15:01:56 +0800

    randomly generate sentence of length random legth

--- test/test_interference.py
+++ test/test_interference.py
@@ -5,16 +5,17 @@ import utils.utility as util
 import random as rd
 import numpy as np
 
-def init_img(font_path, min_font_size, max_font_size,  canvas_width, canvas_height, txt_path, sen_len):
+def init_img(font_path, min_font_size, max_font_size,  canvas_width, canvas_height, txt_path, min_sen_len, max_sen_len):
     font_size = rd.randint(min_font_size, max_font_size)
     printer = Printer(font_path, font_size)
-    img = printer.print_one(canvas_width, canvas_height, read_txt(txt_path, sen_len))
+    # sen_len = rd.randint(min_sen_len, max_sen_len)
+    img = printer.print_one(canvas_width, canvas_height, read_txt(txt_path, min_sen_len, max_sen_len))
     return img
 
 # test read file
-def read_txt(txt_path, sen_len):
+def read_txt(txt_path, min_sen_len,max_sen_len):
     # 句子长度
-    # sen_len = 10
+    sen_len = rd.randint(min_sen_len, max_sen_len)
     random_sen = ""
     # 把汉字输入进一个数组中
     words_list = []
@@ -30,17 +31,25 @@ def read_txt(txt_path, sen_len):
     for le in range(sen_len):
         r = rd.randint(0, words_list.__len__() - 1)
         te = words_list[r]
-        random_sen = str(te) + random_sen
+        random_sapce = rd.randint(0, 10)
+        if random_sapce < 3:
+            random_sen = str(te) + " "+ random_sen
+        else:
+            random_sen = str(te) + random_sen
     return random_sen
     f.close()
 
 if __name__ == '__main__':
-    config = util.read_config('config/template.json')
+    config = util.read_config('config/rotation.json')
     ops = config['ops']
+    file_index = rd.randint(0, 3)
+    # original_im = init_img(config['font']['files'][file_index], config['font']['min_size'], config['font']['max_size'], config['canvas']['width'], config['canvas']['height'], config['char_path']['path'], config['char_path']['min_sen_len'], config['char_path']['max_sen_len'])
 
-    original_im = init_img(config['font']['files'][0], config['font']['min_size'], config['font']['max_size'], config['canvas']['width'], config['canvas']['height'], config['char_path']['path'], config['char_path']['sen_len'])
-
-    for i in range(5):
+    for i in range(10):
+        original_im = init_img(config['font']['files'][file_index], config['font']['min_size'],
+                               config['font']['max_size'], config['canvas']['width'], config['canvas']['height'],
+                               config['char_path']['path'], config['char_path']['min_sen_len'],
+                               config['char_path']['max_sen_len'])
         im = np.copy(original_im)
         angle = 0
         for op, p in ops:
@@ -52,4 +61,4 @@ if __name__ == '__main__':
             im, val = rs
             if isinstance(op, RandomRotation):
                 angle = val
-        uimg.save("data/%d_%.4f.png" % (i, angle), uimg.reverse(im))
+        uimg.save("data/%d_%.4f.jpg" % (i, angle), uimg.reverse(im))
