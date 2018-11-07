fb532c8d6e4f62384e38fc2294042341e6918ae8
bufapiqi <26716594@qq.com>
2018-11-08 00:53:27 +0800

    角度问题修正
    大部分图片基本都能正确旋转到正角度。
    图片的二值化，阈值也是存在一些问题，可能会造成频域图中心出现大量光团，影响霍夫检测
    后续还要再完善

--- angle_transfer.py
+++ angle_transfer.py
@@ -1,7 +1,5 @@
 import cv2
 import numpy as np
-import math
-import sys
 
 
 def image_padded(image):
@@ -12,7 +10,7 @@ def image_padded(image):
     left_right = width - np.size(img_matrix, 1)
     new_matrix = cv2.copyMakeBorder(img_matrix, 0, top_bottom,
                                     0, left_right, borderType=cv2.BORDER_CONSTANT,
-                                    value=255)
+                                    value=0)
     return new_matrix
 
 
@@ -24,23 +22,38 @@ def image_dft(image):
     forier_matrix_magnitude = np.log(np.abs(forier_matrix_shift))
     # 二值化
     forier_matrix_magnitude = forier_matrix_magnitude.astype(np.uint8)
-    ret, threshold_matrix = cv2.threshold(forier_matrix_magnitude, 11, 255, cv2.THRESH_BINARY)
+    ret, threshold_matrix = cv2.threshold(forier_matrix_magnitude, 11, 255, cv2.THRESH_BINARY)   #  11这个阈值 可能需要根据情况变换
+    cv2.imshow("wwq", threshold_matrix)
     # 霍夫直线变换
-    lines = cv2.HoughLinesP(threshold_matrix, 2, np.pi/180, 30, minLineLength=40, maxLineGap=100)
+    lines = cv2.HoughLinesP(threshold_matrix, 2, np.pi/180, 30, minLineLength=0, maxLineGap=100)
+    print(lines)
     return lines
 
 
 def calculate_angle(lines, image):
     height, width = image.shape[:2]
+    angle = 0.0
+    piThresh = np.pi/90
+    pi2 = np.pi/2
     for line in lines:
         x1, y1, x2, y2 = line[0]
-        if x2 - x1 == 0:
+        theta = abs(np.arctan2(y2 - y1, x2 - x1))   # 如果不是绝对值  有可能angle是负的  即 p2 比 p1 小
+        lines_direction = np.arctan2(y2 - y1, x2 - x1)
+        if abs(theta) < piThresh or abs(theta - pi2) < piThresh:
             continue
         else:
-            theta = (y2 - y1) / (x2 - x1)
-    angle = math.atan(theta)
+            angle = theta
+            break
+    if angle >= pi2:
+        angle = angle - np.pi
+    if angle != pi2:
+        anglet = width * np.tan(angle) / height
+        angle = np.arctan(anglet)
     angle = angle * (180 / np.pi)
-    angle = (angle - 90) / (width / height)
+    if lines_direction > 0:    # 方向判断，还需要改进一下
+        angle = angle - 90
+    else:
+        angle = 90 - angle
     return angle
 
 
@@ -55,8 +68,7 @@ def image_rotated(angle, image):
 
 
 if __name__ == '__main__':
-    # 读取图片: cv2.imread(路径,num) 其中num=0，为灰度图像；num=1为彩图
-    img = cv2.imread('E:/ff.png')
+    img = cv2.imread('E:/fuliye/imageText_05_R.jpg')
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     padded_matrix = image_padded(gray)
     hough_lines = image_dft(padded_matrix)
@@ -64,6 +76,7 @@ if __name__ == '__main__':
     result_image = image_rotated(des_angle, img)
     cv2.imshow('current_image', img)
     cv2.imshow('des_image', result_image)
+    # cv2.imwrite("E:/fqq.png", result_image)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
